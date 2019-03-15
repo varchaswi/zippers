728e8fc89ff0e870bcf9f83c480844ba49323b4e
varchaswi <gandhamvarchaswi@gmail.com>
2019-03-15 15:19:15 +0530

    websockets

--- mqtt/broker.js
+++ mqtt/broker.js
@@ -1,10 +1,16 @@
 var mosca = require('mosca');
 var settings = {
-		port:1883
-		}
+	port: 1883,
+	http: {
+		port: 8080,
+		bundle: true,
+		static: './'
+	}
+}
+
 
 var server = new mosca.Server(settings);
 
-server.on('ready', function(){
-console.log("ready");
+server.on('ready', function () {
+	console.log("ready");
 });
